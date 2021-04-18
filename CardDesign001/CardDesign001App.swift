92501295f9371cf2b9cfe43ae53f53c98413500d
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-04-18 03:19:08 -0700

    Initial Commit

---
+++ CardDesign001/CardDesign001App.swift
@@ -0,0 +1,17 @@
+//
+//  CardDesign001App.swift
+//  CardDesign001
+//
+//  Created by Vadiraj Hippargi on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CardDesign001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardDesign001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardDesign001
+//
+//  Created by Vadiraj Hippargi on 4/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
